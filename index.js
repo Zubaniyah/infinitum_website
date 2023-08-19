c4b0ff05d967a1ed9788e9d388a4239e23f6c7f4
MahidulIslam1 <school128311293@gmail.com>
2023-08-19 12:27:59 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const carousel = document.getElementById('carousel');
+const images = [
+  "morning ish.jpeg",
+  "school.jpeg",
+  "new dark.jpeg",
+  "Timeline January - February.png"
+];
+const numberOfImages = images.length;
+let currentImageIndex = 0;
+
+function updateCarousel() {
+  const previousIndex = (currentImageIndex - 1 + numberOfImages) % numberOfImages;
+  const nextIndex = (currentImageIndex + 1) % numberOfImages;
+
+  document.getElementById('previous').children[0].src = images[previousIndex];
+  document.getElementById('current').children[0].src = images[currentImageIndex];
+  document.getElementById('next').children[0].src = images[nextIndex];
+}
+
+setInterval(() => {
+  currentImageIndex = (currentImageIndex + 1) % numberOfImages;
+  updateCarousel();
+}, 4000); 
+
+updateCarousel(); 
